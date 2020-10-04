701afadeec680864df8e846780b139a48a7f2e27
Dylan Turnbull <dylanturn@gmail.com>
2020-10-04 17:04:23 -0500

    forgot to index the ingress output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "traefik_ingress_loadbalancer_ip_list" {
-  value = var.traefik == null ? null : module.traefik.ingress_loadbalancer_ip_list
+  value = var.traefik == null ? null : module.traefik.0.ingress_loadbalancer_ip_list
 }
