ff5854f573e84b2a0ff882b3daa59ed4dff2146b
Dylan Turnbull <dylanturn@gmail.com>
2021-03-05 08:56:31 -0600

    remove traefik lb ip output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "ingress_loadbalancer_ip_list" {
-  value = var.traefik == null ? [] : module.traefik.0.ingress_loadbalancer_ip_list
-}
+#output "ingress_loadbalancer_ip_list" {
+#  value = var.traefik == null ? [] : module.traefik.0.ingress_loadbalancer_ip_list
+#}
 output "cluster_cert_issuer" {
   value = var.cert_manager == null ? "" : module.cert_manager[0].cert_issuer
 }
