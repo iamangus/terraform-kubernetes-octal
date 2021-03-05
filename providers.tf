4408447b7caaff4a784112cff4faa9a1f0d68ba8
Dylan Turnbull <dylanturn@gmail.com>
2021-03-05 09:16:21 -0600

    allow load from config

--- providers.tf
+++ providers.tf
@@ -1,9 +1,11 @@
 provider "kubernetes" {
+   load_config_file = true
   host                   = var.cluster_endpoint
   token                  = var.cluster_token
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
 provider "k8s" {
+
   host                   = var.cluster_endpoint
   token                  = var.cluster_token
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
