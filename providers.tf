6420fcfdf4e3444df9092a1f08ef5a63564be50f
Dylan Turnbull <dylanturn@gmail.com>
2021-03-05 07:59:18 -0600

    removed load_config option

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 provider "kubernetes" {
-  load_config_file       = false
   host                   = var.cluster_endpoint
   token                  = var.cluster_token
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
 provider "k8s" {
-  load_config_file       = false
   host                   = var.cluster_endpoint
   token                  = var.cluster_token
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
