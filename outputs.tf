4f95e0229fe4be5e65cffd269a6604eb9bbf9e22
iamangus <turnbull.angus@gmail.com>
2021-05-29 17:28:00 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 #output "ingress_loadbalancer_ip_list" {
 #  value = var.traefik == null ? [] : module.traefik.0.ingress_loadbalancer_ip_list
 #}
-#output "cluster_cert_issuer" {
-#  value = var.cert_manager == null ? "" : module.cert_manager[0].cert_issuer
-#}
+output "cluster_cert_issuer" {
+  value = var.cert_manager == null ? "" : module.cert_manager[0].cert_issuer
+}
 output "cluster_ingress_class" {
   value = var.traefik == null ? "" : module.traefik[0].ingress_class
 }
